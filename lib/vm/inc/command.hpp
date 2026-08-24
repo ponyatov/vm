7ff58f8d39c85cc06e23bf6ae14d2858327aa5f1
Dmitry Ponyatov <dponyatov@gmail.com>
2026-08-24 19:17:54 +0400

    .

--- lib/vm/inc/command.hpp
+++ lib/vm/inc/command.hpp
@@ -6,10 +6,14 @@
 
 #include "lib.hpp"
 
-enum class Op : uint8_t {  //
+enum class Op : uint8_t {
     nop = 0x00,
     halt = 0xFF,
     debug = 0xDE,
 };
+
+extern void nop();    ///< `( -- )` do nothing (empty command)
+extern void halt();   ///< `( -- )` stop the whole @ref vm
+extern void debug();  ///< `( -- )` dump @ref vm state
 
 /// @}
